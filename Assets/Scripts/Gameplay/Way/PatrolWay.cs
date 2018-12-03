7c5f0b9bee1f657fbcf28561c3d244587f4776c4
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2018-12-03 04:58:46 +0300

    Modify PatrolWay (add work in Editor)

--- Assets/Scripts/Gameplay/Way/PatrolWay.cs
+++ Assets/Scripts/Gameplay/Way/PatrolWay.cs
@@ -1,33 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[ExecuteInEditMode]
 public class PatrolWay : MonoBehaviour {
-    public List<PatrolWayPoint> Points = new List<PatrolWayPoint>();
+    public PatrolWayPoint[] Points;
     [SerializeField]
     private int _index = 0;
 
     public void Start()
     {
         _index = 0;
-        if (Points.Count == 0)
-        {
-            Debug.LogError("PointsWay is null");
-        }
-        for(int i = 0; i < Points.Count; i++)
+        Points = gameObject.GetComponentsInChildren<PatrolWayPoint>();
+        for (int i = 0; i < Points.Length; i++)
         {
             Points[i].ID = i;
         }
     }
 
-    public List<PatrolWayPoint> GetPoints()
-    {
-        return Points;
-    }
-
     public PatrolWayPoint GetCurrentPoint()
     {
-        if (_index >= 0 && _index < Points.Count)
+        if (_index >= 0 && _index < Points.Length)
         {
             return Points[_index];
         }
@@ -36,7 +27,7 @@ public class PatrolWay : MonoBehaviour {
 
     public PatrolWayPoint GetPointByIndex(int index)
     {
-        if (index > 0 && index < Points.Count)
+        if (index > 0 && index < Points.Length)
         {
             _index = index;
             return Points[index];
@@ -47,7 +38,7 @@ public class PatrolWay : MonoBehaviour {
     private PatrolWayPoint GetNextPoint()
     {
         _index++;
-        if (_index >= Points.Count)
+        if (_index >= Points.Length)
         {
             _index = 0;
         }
@@ -83,7 +74,7 @@ public class PatrolWay : MonoBehaviour {
 
     void OnDrawGizmos()
     {
-        for (int i = 0; i < Points.Count; i++)
+        for (int i = 0; i < Points.Length; i++)
         {
             Gizmos.color = (i==_index) ? Color.green : Color.blue;
             Gizmos.DrawSphere(Points[i].transform.position, 3f);
@@ -93,7 +84,7 @@ public class PatrolWay : MonoBehaviour {
             }
             else
             {
-                Gizmos.DrawLine(Points[Points.Count - 1].transform.position, Points[0].transform.position);
+                Gizmos.DrawLine(Points[Points.Length - 1].transform.position, Points[0].transform.position);
             }
         }
     }
