c6abcfa7b47cf65a59753a5073bfbe44f9d1c46a
boronnikovAI <boronnikov_a_i@mail.ru>
2019-06-19 20:45:59 +0300

    Мы новый мир построим на руинах старого

--- Assets/Scripts/Gameplay/Way/PatrolWayPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PatrolWayPoint : MonoBehaviour {
-    public int ID = -1;
-
-}
--- Assets/Scripts/Units/IModification.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IModification
-{
-    void StartMode();
-    void UpdateMode();
-}
