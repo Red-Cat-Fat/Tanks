9e0f28c033502e188057ada924ba2186ed81b237
boronnikovAI <boronnikov_a_i@mail.ru>
2019-06-21 21:12:07 +0300

    Заготовка для оружия

---
+++ Assets/Scripts/Game/Controllers/Units/GunControllers/BaseGunController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Controllers.Units.GunControllers
+{
+	public class BaseGunController : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		private void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+
+		}
+	}
+}
