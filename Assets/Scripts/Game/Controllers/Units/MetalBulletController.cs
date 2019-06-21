e65566e243e3116ae01a1aae08caba5fa11f80f1
boronnikovAI <boronnikov_a_i@mail.ru>
2019-06-21 17:57:31 +0300

    Добавил контроллер для пули

---
+++ Assets/Scripts/Game/Controllers/Units/MetalBulletController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Controllers.Units
+{
+	public class MetalBulletController : MonoBehaviour, IController
+	{
+		public Vector3 GetNewTargetPosirionVector3()
+		{
+			return transform.forward;
+		}
+	}
+}
