60e650ecf0929047ec1495d5be0df3b209bc86bf
boronnikovAI <boronnikov_a_i@mail.ru>
2019-06-21 17:26:07 +0300

    Добавил задний ход

--- Assets/Scripts/Game/Systems/MoveSystem.cs
+++ Assets/Scripts/Game/Systems/MoveSystem.cs
@@ -8,7 +8,8 @@ namespace Game.Systems
 {
 	public class MoveSystem : MonoBehaviour
 	{
-		[SerializeField] private float _speed = 3f;
+		[SerializeField] private float _speedMove = 3f;
+		[SerializeField] private float _speedRotate = 3f;
 		private IController _unitsController;
 		private Rigidbody _unitsRigidbody;
 
@@ -35,17 +36,25 @@ namespace Game.Systems
 		private void Move(float deltaTime)
 		{
 			var newPositionVector3 = _unitsController.GetNewTargetPosirionVector3();
+			if (newPositionVector3 == transform.position) return;
+
 			var directionVector3 = newPositionVector3 - transform.position;
 			var distanceToTargetPoint = directionVector3.magnitude;
-			var timeMoveToPoint = distanceToTargetPoint / _speed;
+			var timeMoveToPoint = distanceToTargetPoint / _speedMove;
 			var procent = deltaTime / timeMoveToPoint;
 			var teleportToPosition = Vector3.Lerp(transform.position, newPositionVector3, procent);
-			Move(teleportToPosition);
+
+			var rotation = Vector3.Dot(transform.forward, directionVector3) <= 0
+				? Quaternion.LookRotation(-directionVector3)
+				: Quaternion.LookRotation(directionVector3);
+			var teleportToRotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * _speedRotate);
+
+			Move(teleportToPosition, teleportToRotation);
 		}
 
-		private void Move(Vector3 newPositionVector3)
+		private void Move(Vector3 newPositionVector3, Quaternion rotationQuaternion)
 		{
-			transform.LookAt(newPositionVector3);
+			_unitsRigidbody.rotation = rotationQuaternion;
 			_unitsRigidbody?.MovePosition(newPositionVector3);
 		}
 	}
