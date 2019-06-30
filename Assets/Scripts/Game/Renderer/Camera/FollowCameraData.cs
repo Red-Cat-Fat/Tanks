792589619f3a157997c48ef1cee792ef876ed472
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2019-06-30 15:26:28 +0300

    Реализовал систему следящей камеры

---
+++ Assets/Scripts/Game/Renderer/Camera/FollowCameraData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game.Renderer.Camera
+{
+	public class FollowCameraData : MonoBehaviour
+	{
+		[SerializeField] private GameObject _anchorPoint;
+		[SerializeField] private GameObject _followObject;
+		[SerializeField] private float _moveSpeed;
+		[SerializeField] private float _rotateSpeed;
+
+		public GameObject GetAnchorPoint()
+		{
+			return _anchorPoint;
+		}
+
+		public GameObject GetFollowObject()
+		{
+			return _followObject;
+		}
+
+		public float GetMoveSpeed()
+		{
+			return _moveSpeed;
+		}
+
+		public float GetRotateSpeed()
+		{
+			return _rotateSpeed;
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Renderer/Camera/FollowCameraSystem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Game.Renderer.Camera
+{
+	public class FollowCameraSystem : MonoBehaviour
+	{
+		private FollowCameraData _followCameraData;
+		private Vector3 _lastAnchorPointPositionVector3;
+		private float _moveTime = 0f;
+
+		private void Start()
+		{
+			_followCameraData = GetComponent<FollowCameraData>();
+			if(_followCameraData == null)
+			{
+				Debug.LogError("FollowCameraData on " + gameObject.name + " is null");
+			}
+		}
+
+		private void FixedUpdate()
+		{
+			var anchorPoint = _followCameraData.GetAnchorPoint();
+			var moveSpeed = _followCameraData.GetMoveSpeed();
+			var followObject = _followCameraData.GetFollowObject();
+
+			if (anchorPoint.transform.position != _lastAnchorPointPositionVector3)
+			{
+				_lastAnchorPointPositionVector3 = anchorPoint.transform.position;
+				_moveTime = 0f;
+			}
+			_moveTime += Time.deltaTime;
+			var progress = _moveTime * moveSpeed;
+			if (progress > 1f)
+			{
+				transform.position = _lastAnchorPointPositionVector3;
+			}
+			else
+			{
+				transform.position = Vector3.Lerp(transform.position, _lastAnchorPointPositionVector3, progress);
+			}
+			var roteteSpeed = _followCameraData.GetRotateSpeed();
+			var rotateProgress = roteteSpeed * _moveTime;
+			LookAt(followObject.transform.position, rotateProgress);
+		}
+
+		private void LookAt(Vector3 position, float progress)
+		{
+			var directionVector3 = position - transform.position;
+			if (directionVector3 == Vector3.zero) return;
+			var rotation = Quaternion.LookRotation(directionVector3);
+			var teleportToRotation = Quaternion.Slerp(transform.rotation, rotation, progress);
+			transform.rotation = teleportToRotation;
+		}
+	}
+}
