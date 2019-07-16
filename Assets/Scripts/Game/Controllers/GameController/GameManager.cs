8524390c6f997171f32db50564cc24be35428fb3
Andrey Boronnikov <boronnikov_a_i@mail.ru>
2019-07-16 20:04:16 +0300

    Сделал список врагов в GameManager

--- Assets/Scripts/Game/Controllers/GameController/GameManager.cs
+++ Assets/Scripts/Game/Controllers/GameController/GameManager.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using Game.HelpModules;
 using UnityEngine;
 
-
 namespace Game.Controllers.GameControllers
 {
 	public class GameManager : SingletonBehaviour<GameManager>
 	{
 		public GameObject PlayerGameObject;
+		private List<GameObject> _enemys;
+
+		public void AddEnemy(GameObject enemy)
+		{
+			_enemys.Add(enemy);
+		}
+
+		private GameObject GetMinDistanceEnemy(Vector3 position)
+		{
+			GameObject returnValue = null;
+			var minDistance = float.MaxValue;
+
+			foreach (var enemy in _enemys)
+			{
+				var distanceVector = enemy.transform.position - position;
+				var sqrMagnitude = distanceVector.sqrMagnitude;
+				if (sqrMagnitude < minDistance)
+				{
+					returnValue = enemy;
+					minDistance = sqrMagnitude;
+				}
+			}
+
+			return returnValue;
+		}
 	}
 }
