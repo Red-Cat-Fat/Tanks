5419c59be278fe0ce8e0adff8960ad0194d637f0
Andrey Boronnikov <boronnikov_a_i@mail.ru>
2019-07-31 18:30:55 +0300

    Отделил TimeLoggerUtility от TimeLoggerUtilityEditorWindow

---
+++ Assets/Scripts/Editor/Utility/Logger/TimeLogSystem/TimeLoggerUtility.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Editor.Utility.Logger.TimeLogSystem;
+using UnityEngine;
+
+namespace Editor.Utility.Logger.TimeLogSystem
+{
+	public class TimeLoggerUtility
+	{
+		private List<Task> _tasks = new List<Task>();
+		private static Task _currentTask;
+
+		public Task GetCurrentTask()
+		{
+			return _currentTask;
+		}
+
+		public List<Task> GetTasksList()
+		{
+			return _tasks;
+		}
+
+		public void TryStartNewTask(string taskName)
+		{
+			if (_currentTask != null)
+			{
+				if (_currentTask.GetName() == taskName
+				    && !_currentTask.IsInProgress())
+				{
+					_currentTask.Start();
+				}
+			}
+			else
+			{
+				_currentTask = new Task(taskName);
+				_currentTask.Start();
+			}
+		}
+
+		public void TryPausedTask()
+		{
+			_currentTask.Pause();
+		}
+
+		public void TryStopedTask()
+		{
+			_currentTask.Stop();
+			_tasks.Add((Task)_currentTask.Clone());
+			_currentTask = null;
+		}
+	}
+}
--- Assets/Scripts/Editor/Utility/Logger/TimeLogSystem/TimeLoggerUtilityEditorWindow.cs
+++ Assets/Scripts/Editor/Utility/Logger/TimeLogSystem/TimeLoggerUtilityEditorWindow.cs
@@ -13,17 +13,15 @@ namespace Editor.Utility.Logger.TimeLogSystem
 			GetWindow<TimeLoggerUtilityEditorWindow>();
 		}
 
-		private List<Task> _tasks = new List<Task>();
-		private static Task _currentTask;
+		private TimeLoggerUtility _timeLoggerUtility = new TimeLoggerUtility();
 		private static string _currentTextInFiledTask = "";
-
 		private void OnGUI()
 		{
 			var now = DateTime.Now;
 			GUILayout.Label($"Current time: {now}");
-
-			GUILayout.Label(_currentTask != null
-				? $"Current task: {_currentTask.GetName()}"
+			var currentTask = _timeLoggerUtility.GetCurrentTask();
+			GUILayout.Label(currentTask != null
+				? $"Current task: {currentTask.GetName()}"
 				: "Current task: null");
 
 			GUILayout.BeginHorizontal();
@@ -32,54 +30,26 @@ namespace Editor.Utility.Logger.TimeLogSystem
 
 			if (GUILayout.Button("Start", GUILayout.Width(50)))
 			{
-				TryStartNewTask();
+				_timeLoggerUtility.TryStartNewTask(_currentTextInFiledTask);
 			}
 
 			if (GUILayout.Button("Pause", GUILayout.Width(50)))
 			{
-				TryPausedTask();
+				_timeLoggerUtility.TryPausedTask();
 			}
 
 			if (GUILayout.Button("Stop", GUILayout.Width(50)))
 			{
-				TryStopedTask();
+				_timeLoggerUtility.TryStopedTask();
 			}
 
 			GUILayout.EndHorizontal();
 
-			foreach (var task in _tasks)
+			foreach (var task in _timeLoggerUtility.GetTasksList())
 			{
 				GUILayout.Label(task.GetName() + ": " + task.GetWorkFromTask());
 			}
 		}
 
-		private void TryStartNewTask()
-		{
-			if (_currentTask != null)
-			{
-				if (_currentTask.GetName() == _currentTextInFiledTask
-				    && !_currentTask.IsInProgress())
-				{
-					_currentTask.Start();
-				}
-			}
-			else
-			{
-				_currentTask = new Task(_currentTextInFiledTask);
-				_currentTask.Start();
-			}
-		}
-
-		private void TryPausedTask()
-		{
-			_currentTask.Pause();
-		}
-
-		private void TryStopedTask()
-		{
-			_currentTask.Stop();
-			_tasks.Add((Task) _currentTask.Clone());
-			_currentTask = null;
-		}
 	}
 }
